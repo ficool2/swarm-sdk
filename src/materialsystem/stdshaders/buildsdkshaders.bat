bc664e8decd1bcb50062fe45b7274154ecde9667
ficool2 <34815548+ficool2@users.noreply.github.com>
2025-02-08 12:45:54 +0000

    Replace ShaderCompile with SCell555's ShaderCompile
    Generate shader includes

--- src/materialsystem/stdshaders/buildsdkshaders.bat
+++ src/materialsystem/stdshaders/buildsdkshaders.bat
@@ -14,12 +14,12 @@ rem == Note that this path needs does not support long file/directory names ==
 rem == So instead of a path such as "C:\Program Files\Steam\steamapps\mymod" ==
 rem == you need to find the 8.3 abbreviation for the directory name using 'dir /x' ==
 rem == and set the directory to something like C:\PROGRA~2\Steam\steamapps\sourcemods\mymod ==
-set GAMEDIR=C:\PROGRA~2\Steam\steamapps\sourcemods\swarmsdktemplate
+set GAMEDIR=C:\src\swarm-sdk\build
 
 rem == Set the relative path to steamapps\common\Alien Swarm\bin ==
 rem == As above, this path does not support long directory names or spaces ==
 rem == e.g. ..\..\..\..\..\PROGRA~2\Steam\steamapps\common\ALIENS~1\bin ==
-set SDKBINDIR=C:\PROGRA~2\Steam\steamapps\common\ALIENS~1\bin
+set SDKBINDIR=C:\src\swarm-sdk\bin
 
 rem ==  Set the Path to your mods root source code ==
 rem this should already be correct, accepts relative paths only!
@@ -48,7 +48,6 @@ set tt_all_start=%ERRORLEVEL%
 set tt_all_chkpt=%tt_start%
 
 set BUILD_SHADER=call buildshaders.bat
-set ARG_EXTRA=
 
 %BUILD_SHADER% stdshader_dx9_20b		-game %GAMEDIR% -source %SOURCEDIR%
 %BUILD_SHADER% stdshader_dx9_20b_new		-game %GAMEDIR% -source %SOURCEDIR% -dx9_30
